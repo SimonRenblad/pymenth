286b4840470f33b5770ca4b76d1fa0b24d0ece72
Simon Renblad <srenblad@m-labs.hk>
2025-09-10 14:41:42 +0800

    answer -> answer_base

--- menth.py
+++ menth.py
@@ -8,25 +8,28 @@ def get_parser():
     parser.add_argument("--seed")
     parser.add_argument("--length", type=int, default=None)
     parser.add_argument("pattern", type=str)
-    parser.add_argument("answer", type=str)
+    parser.add_argument("-b", "--answer_base", type=int, default=10)
     return parser
 
 
 BASE = {2: "b", 8: "o", 10: "d", 16: "x"}
 PREFIX = {2: "0b", 8: "0o", 10: "", 16: "0x"}
+OPERATORS = "+-*/"
+
 
 class Test:
-    def __init__(self, pattern, answer, length=None):
+    def __init__(self, pattern, answer_base, length=None):
         self.operands = []
         self.operators = []
-        self.answer = int(answer)
+        assert answer_base in BASE.keys()
+        self.answer_base = answer_base
         self.length = length
         self.curr = 0
         p = pattern.split(",")
         for i, o in enumerate(p):
             if i != 0:
                 opr = o[0]
-                assert opr in "+-"
+                assert opr in OPERATORS
                 o = o[1:]
                 self.operators.append(opr)
             rnge = o.split(":")
@@ -64,21 +67,26 @@ class Test:
                     answer += val
                 elif opr == "-":
                     answer -= val
+                elif opr == "*":
+                    answer *= val
+                elif opr == "/":
+                    # we don't care about float div
+                    answer //= val
         self.curr_answer = answer
         return question
 
     def check_answer(self, answer):
-        val = int(answer, base=self.answer)
+        val = int(answer, base=self.answer_base)
         return val == self.curr_answer
 
     def get_answer(self):
-        return (PREFIX[self.answer] + "{:" + BASE[self.answer] + "}").format(self.curr_answer)
+        return (PREFIX[self.answer_base] + "{:" + BASE[self.answer_base] + "}").format(self.curr_answer)
 
 
 def main():
     args = get_parser().parse_args()
     random.seed(time.time())
-    test = Test(args.pattern, args.answer, length=args.length)
+    test = Test(args.pattern, args.answer_base, length=args.length)
     while True:
         q = test.get_question()
         if q is None:
@@ -88,9 +96,12 @@ def main():
             usr_ans = input()
         except KeyboardInterrupt:
             return
-        if test.check_answer(usr_ans):
-            print("correct")
-        else:
+        try:
+            if test.check_answer(usr_ans):
+                print("correct")
+            else:
+                print("incorrect, answer was " + test.get_answer())
+        except:
             print("incorrect, answer was " + test.get_answer())
 
 
